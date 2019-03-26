e84f003fb1837ba26dca121fffd6a83401d83ced
Alejandro Romero Herrera <alromh87@gmail.com>
2019-03-26 12:01:52 +0100

    temporal

--- oneArm.py
+++ oneArm.py
@@ -25,73 +25,28 @@ def main():
 #    goal.orientation.x = 0
 #    goal.orientation.y = 0
 #    goal.orientation.z = 1.57
+
+    print "Initial pose:"
+    print one_arm.get_current_pose().pose
     
-    for x in range(0,5):
-        goal.position.x = .7
-        goal.position.y = 0
-        goal.position.z = 1
+    for x in range(0,1):
+#        goal.position.x = .7
+#        goal.position.y = 0
+#        goal.position.z = 1
+        goal.position.x = 0.817434355362
+        goal.position.y = 0.0259007808625
+        goal.position.z = 1.19833020259
         
+        print "Trying to get to:"
         print goal
         
         one_arm.set_pose_target(goal)
         plan1 = one_arm.plan()
         one_arm.go(wait=True)
-        
-#        #rospy.sleep(5)
-#        
-#        goal.position.x+=.29
-#        goal.position.y-=.415
-#        goal.position.z-=.87
-#        one_arm.set_pose_target(goal)
-#        print "ya"
-#        plan1 = one_arm.plan()
-#        one_arm.go(wait=True)
-#        
-#        goal.position.x = .7
-#        goal.position.y = 0
-#        goal.position.z = 1
-#        one_arm.set_pose_target(goal)
-#        plan1 = one_arm.plan()
-#        one_arm.go(wait=True)
-        
-        goal.position.x = random.uniform(.7,1.1)
-        goal.position.y = random.uniform(-.4,.4)
-        goal.position.z = .2
-        one_arm.set_pose_target(goal)
-        print "goal = (" + str(goal.position.x) + ", " + str(goal.position.y) + ")"
-        plan1 = one_arm.plan()
-        one_arm.go(wait=True)
-        
-        goal.position.z = .1
-        one_arm.set_pose_target(goal)
-        plan1 = one_arm.plan()
-        one_arm.go(wait=True)
-        
-        goal.position.z = .2
-        one_arm.set_pose_target(goal)
-        plan1 = one_arm.plan()
-        one_arm.go(wait=True)
+        print "Pose:"
+        print one_arm.get_current_pose().pose
 
         
-#    for x in range(0,4):
-#        goal.position.z-=.15
-#        one_arm.set_pose_target(goal)
-#        print "ya"
-#        plan1 = one_arm.plan()
-#        one_arm.go(wait=True)
-
-    goal.position.x = .7
-    goal.position.y = 0
-    goal.position.z = 1
-    
-    print goal
-    
-    one_arm.set_pose_target(goal)
-    plan1 = one_arm.plan()
-    one_arm.go(wait=True)
-        
-    print one_arm.get_current_pose().pose
-
 if __name__ == '__main__':
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('goalsender')
